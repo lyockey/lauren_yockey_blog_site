55e82149cd05e69c3b36cfeb5131b207b215ce68
Lauren Yockey <laurenyockey@gmail.com>
2016-10-24 10:16:35 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!-- Begin Footer -->
+    <footer class="row">
+
+      <div class="four columns">
+        <?php dynamic_sidebar('footer-widget-one'); ?>
+      </div>
+
+      <div class="four columns">
+        <?php dynamic_sidebar('footer-widget-two'); ?>
+      </div>
+
+      <div class="four columns">
+        <?php dynamic_sidebar('footer-widget-three'); ?>
+      </div>
+
+
+    </footer>
+
+  </div><!--End Container Div-->
+  <?php wp_footer(); ?>
+
+</body>
+</html>
+<!-- End Footer -->
