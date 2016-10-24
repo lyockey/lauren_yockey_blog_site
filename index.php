77c1dd6499e6d84718ed8a00fdcac034690c77e6
Lauren Yockey <laurenyockey@gmail.com>
2016-10-24 17:01:32 -0500

    put intro copy in new row with 7 columns

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
                 <?php if ( has_post_thumbnail() ) { ?>
 					<div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
 				<?php } ?>
+            </div>
+        <section class="row">
+            <div class="seven columns"
                 <p id="introcopy">A collection of work from the Web Interactivity &amp; Engagement course as part of the University of Florida's WDOC program</p>
             </div>
         </section>
