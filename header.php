fed072ecd3b9bf047e6b335561a165be45dd7c7c
Lauren Yockey <laurenyockey@gmail.com>
2016-10-24 11:34:21 -0500

    separate template files

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WIE - Lauren Yockey</title>
+
+        <!-- Links to our Style.css file -->
+    	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+
+    </head>
+<body>
+    <div class="container">
+        <header class="row">
+            <div class="twelve columns">
+                <h1>WIE</h1>
+                <p>lauren yockey</p>
+            </div>
+        </header>
+        <!-- END OF HEADER - BEGIN SECTION CONTENT -->
--- index.php
+++ index.php
@@ -1,31 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>WIE - Lauren Yockey</title>
+<?php get_header(); ?>
 
-        <!-- Links to our Style.css file -->
-    	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-
-    </head>
-<body>
-    <div class="container">
-        <header class="row">
-            <div class="twelve columns">
-                <h1>WIE</h1>
-                <p>lauren yockey</p>
-            </div>
-        </header>
         <section class="row">
             <div class="twelve columns">
                 <p>A collection of work from the Web Interactivity &amp; Engagement course as part of the University of Florida's WDOC program</p>
             </div>
         </section>
-        <footer class="row">
-            <div class="twelve columns">
-                <p>&copy; Lauren Yockey 2016. University of Florida</p>
-            </div>
-        </footer>
-    </div>
-</body>
-</html>
+
+<?php get_footer(); ?>
