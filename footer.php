3cd3c19f12e5d931b0cddbcc29b939ea56e9acfc
Lauren Yockey <laurenyockey@gmail.com>
2016-10-24 16:46:56 -0500

    reverse footer with container div

--- footer.php
+++ footer.php
@@ -1,10 +1,15 @@
+</div>
+
+<div class="footer-wrapper">
+    <footer class="container">
 <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
-        <footer class="row">
+        <div class="row">
             <div class="twelve columns">
                 <p>&copy; Lauren Yockey 2016. University of Florida</p>
             </div>
-        </footer>
-    </div> <!-- ends container -->
+        </div>
+    </footer>
+</div> <!-- ends container -->
 <?php wp_footer(); ?>
 </body>
 </html>
