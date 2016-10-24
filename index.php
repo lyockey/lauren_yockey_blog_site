bd93b83e8d163f401e7e55cd1c2e60d779096bfb
Lauren Yockey <laurenyockey@gmail.com>
2016-10-24 11:03:20 -0500

    add charset to head

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>WIE - Lauren Yockey</title>
+
         <!-- Links to our Style.css file -->
     	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+
     </head>
 <body>
     <h1>WIE</h1>
