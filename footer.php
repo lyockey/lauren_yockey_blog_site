d84fb386b206ce02df612836274d470f6cb7f30f
Lauren Yockey <laurenyockey@gmail.com>
2016-10-24 21:31:55 -0500

    indent footer contents

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-</div>
+    </div>
 
-<div class="footer-wrapper">
-    <footer class="container">
-<!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
-        <div class="row">
-            <div class="twelve columns">
-                <p>&copy; Lauren Yockey 2016. University of Florida</p>
+    <div class="footer-wrapper">
+        <footer class="container">
+    <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
+            <div class="row">
+                <div class="twelve columns">
+                    <p>&copy; Lauren Yockey 2016. University of Florida</p>
+                </div>
             </div>
-        </div>
-    </footer>
-</div> <!-- ends container -->
-<?php wp_footer(); ?>
+        </footer>
+    </div> <!-- ends container -->
+    <?php wp_footer(); ?>
 </body>
 </html>
