e7f0b46533b7fa5eb9f068fcc629411e98c8cedf
Lauren Yockey <laurenyockey@gmail.com>
2016-10-25 14:40:47 -0500

    add banner image to home page

--- index.php
+++ index.php
@@ -2,13 +2,17 @@
 
         <section class="row">
             <div class="twelve columns">
-                <?php if ( has_post_thumbnail() ) { ?>
-					<div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
-				<?php } ?>
+                <div class="banner">
+                    <p>Web Interactivity</p>
+                    <p> &amp; Engagement</p>
+                </div>
             </div>
         <section class="row">
-            <div class="seven columns"
+            <div class="eight columns"
                 <p id="introcopy">A collection of work from the Web Interactivity &amp; Engagement course as part of the University of Florida's WDOC program</p>
+            </div>
+            <div class="four columns">
+                <?php get_sidebar(); ?>
             </div>
         </section>
 
