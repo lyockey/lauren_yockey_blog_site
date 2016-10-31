8b35200bb5be8d23f89729c84af957cdb7a4d816
Lauren Yockey <laurenyockey@gmail.com>
2016-10-31 10:23:11 -0500

    add posts to home page

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<!-- HEADER AREA -->
         <section class="row">
             <div class="twelve columns">
                 <div class="banner">
@@ -7,10 +8,44 @@
                     <p> &amp; Engagement</p>
                 </div>
             </div>
+
+<!-- INTRO COPY -->
         <section class="row">
-            <div class="eight columns"
+            <div class="eight columns">
                 <p id="introcopy">A collection of work from the Web Interactivity &amp; Engagement course as part of the University of Florida's WDOC program</p>
             </div>
+        </section>
+<!-- FEATURED POST -->
+              <!-- Begin Loop -->
+      <section class="row">
+          <div class ="six columns">
+                  <?php
+                      if ( have_posts() ) {
+                        while ( have_posts() ) {
+                          the_post(); ?>
+                          <?php
+                                if ( has_post_thumbnail() ) {
+                                    the_post_thumbnail('thumbnail');
+                                }
+                            ?>
+                            <h3>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h3>
+                            <?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>">Read More</a>
+
+                            <?php
+                        } //end while
+                      } //end if
+                  ?>
+
+                  <!-- End Loop -->
+                </div>
+            </section>
+<!-- End Section Container -->
+        <section class="row">
             <div class="four columns">
                 <?php get_sidebar(); ?>
             </div>
