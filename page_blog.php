2ba2ae32df189b02a2fd1cbb4dd3104959c255a1
Lauren Yockey <laurenyockey@gmail.com>
2016-11-01 14:33:00 -0500

    enable blog posts on blog page

--- page_blog.php
+++ page_blog.php
@@ -1,40 +1,45 @@
 <?php
 /*
-Template name: Blog
+Template name: Blog Posts
 */
 ?>
 
 <?php get_header(); ?>
+<?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
 
 <!-- Begin Section Container -->
 <section class="row">
-  <div class="twelve columns">
-      <img src="http://laurenyockey.com/wie/wp-content/uploads/2016/11/blog_banner.png" alt="Contact" id="blog-banner" />
-  </div>
+    <div class="twelve columns">
+        <img src="http://laurenyockey.com/wie/wp-content/uploads/2016/11/blog_banner.png" alt="Contact" id="blog-banner" />
+    </div>
 </section>
 <section class="row">
-  <div class="twelve columns">
-      <?php
-          wp_nav_menu( array( 'theme_location' => 'main-menu', 'container_class' => 'nav-container' ) );
-      ?>
-  </div>
+    <div class="twelve columns">
+        <?php
+            wp_nav_menu( array( 'theme_location' => 'main-menu', 'container_class' => 'nav-container' ) );
+        ?>
+    </div>
 </section>
 <section class="row">
-  <div class="eight columns">
-      <h2 class="page-heading">Blog Posts</h2>
-      <!-- BEGIN LOOP -->
-			<?php
-			if ( have_posts() ) {
-			    while ( have_posts() ) {
-			        the_post();
-			    } // end while
-			} // end if
-			?>
-<!-- END LOOP -->
-  </div>
-  <div class="four columns">
-      <?php get_sidebar(); ?>
-  </div>
+    <div class="eight columns">
+        <h2 class="page-heading">Blog Posts</h2>
+    <!-- BEGIN LOOP -->
+    <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content();
+            endwhile; ?>
+        <!-- Navigation -->
+        <div class="navigation">
+            <span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+        </div>
+        <?php endif;
+        wp_reset_query(); ?>
+    <!-- END LOOP -->
+    </div>
+    <div class="four columns">
+        <?php get_sidebar(); ?>
+    </div>
 </section>
 <!-- End Section Container -->
 
