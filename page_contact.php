10d1857e7a79ad86c529946fe68502937516fb43
Lauren Yockey <laurenyockey@gmail.com>
2016-11-01 13:56:45 -0500

    add buffer space between sections

--- page_contact.php
+++ page_contact.php
@@ -13,6 +13,7 @@ Template name: Contact
             <img src="http://laurenyockey.com/wie/wp-content/uploads/2016/11/contact_banner.png" alt="Contact" id="contact-banner"/>
         </div>
     </section>
+
     <section class="row"
         <div class="twelve columns">
             <?php
@@ -20,7 +21,8 @@ Template name: Contact
             ?>
         </div>
     </section>
-      <section class="row">
+
+    <section class="row">
         <div class="eight columns">
             <h2 class="page-heading">Contact</h2>
         </div>
@@ -29,7 +31,7 @@ Template name: Contact
             <?php get_sidebar(); ?>
         </div>
 
-      </section>
+    </section>
   <!--End Section Container -->
 
 <?php get_footer(); ?>
