17aa3e919bd97fb4785bc7c5f2bdcb0d9019e065
Lauren Yockey <laurenyockey@gmail.com>
2016-11-05 21:28:22 -0500

    finalize global styles

--- 404.php
+++ 404.php
@@ -6,13 +6,13 @@ get_header(); ?>
     <section class="row 404-page">
         <div class="eight columns">
             <h2 class="page-heading">Not Found</h2>
-                <h3 class="">This is somewhat embarrassing, isn't it?</h2>
+                <h3 class="404-heading">This is somewhat embarrassing, isn't it?</h3>
                     <p>It looks like nothing was found at this location. Maybe try a search?</p>
 
-                <h4>Try searching for what you need:</h4>
+                <h4 class="404-try-search">Try searching for what you need:</h4>
                 <?php get_search_form(); ?>
         </div>
-        <div class="four columns">
+        <div class="four columns sidebar">
             <?php get_sidebar(); ?>
         </div>
 
